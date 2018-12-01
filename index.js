2896447eac8211cfba3bb269a62c369a579f3b77
achmadTheCoder <thinkhover@gmail.com>
2018-12-01 21:50:55 +0700

    managing window

--- index.js
+++ index.js
@@ -3,7 +3,26 @@ var browserWindow = electron.BrowserWindow;
 var app = electron.app;
 
 app.on('ready', function(){
-  var appWindow;
-  appWindow = new browserWindow();
+  var appWindow, infoWindow;
+  appWindow = new browserWindow({
+    show:false
+  });
   appWindow.loadURL('http://www.google.com');
+
+  infoWindow = new browserWindow({
+    width:400,
+    height:300,
+    transparent:true,
+    frame:false,
+    show:false
+  });
+  infoWindow.loadURL('file://'+ __dirname + '/info.html');
+  appWindow.once('ready-to-show',function(){
+    appWindow.show();
+    setTimeout(function(){
+      infoWindow.show();
+      setTimeout(function(){infoWindow.hide();},3000);
+    },3000);
+  });
 })
+
