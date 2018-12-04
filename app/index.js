6e2cdb75e3d60dede5f673ee4b91b98dd7f182e5
achmadTheCoder <thinkhover@gmail.com>
2018-12-05 05:30:10 +0700

    bismillah

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 var electron = require('electron');
 var browserWindow = electron.BrowserWindow;
 var app = electron.app;
+var ipc = electron.ipcMain;
+
 
 app.on('ready', function(){
   var appWindow, infoWindow;
@@ -9,20 +11,27 @@ app.on('ready', function(){
   });
   appWindow.loadURL('http://raybo.org');
 
-  infoWindow = new browserWindow({
+  infoWindow = new browserWindow({ //infowindows
     width:400,
     height:300,
     transparent:true,
     frame:false,
     show:false
   });
+
   infoWindow.loadURL('file://'+ __dirname + '/info.html');
-  appWindow.once('ready-to-show',function(){
+
+  appWindow.once('ready-to-show',function(){ //ready to show main app window
     appWindow.show();
     setTimeout(function(){
       infoWindow.show();
       // setTimeout(function(){infoWindow.hide();},3000);
     },1000);
   });
-});
+
+  ipc.on('closeInfoWindow', function(event){
+    event.returnValue='';
+    infoWindow.hide();
+  }) //close window
+});//app is ready
 
